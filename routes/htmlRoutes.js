0bb6b48755cd10f33f4933f05d1f4acf9442305c
David Gutierrez <dcgutierrez93@yahoo.com>
2018-11-16 13:10:11 -0600

    Added Routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,8 @@
+// Dependencies
+const path = require('path');
+
+module.exports = function(app) {
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/views/home.html"));
+    })
+}
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 // Dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// bodyParser
+app.use(bodyParser.json());
+
+// Static directory
+app.use(express.static(__dirname + '/public'));
+
+// Require Routes
+require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
