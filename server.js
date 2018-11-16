3aa806843f493180a464fb39e43ec88b77101379
David Gutierrez <dcgutierrez93@yahoo.com>
2018-11-16 13:03:50 -0600

    Setup Express Server

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
